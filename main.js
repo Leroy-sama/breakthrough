e396da458130da54f6d83bdbae451abb72fbfa98
Leroy Mogire <ntruderleroy@gmail.com>
2023-08-31 08:24:10 +0300

    fix - a click on the links removes navbar

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active')
     navLinks.classList.toggle('active')
 })
+
+document.querySelectorAll(".nav-link").forEach((n) => {
+    n.addEventListener('click', () => {
+        hamburger.classList.remove('active')
+        navLinks.classList.remove('active')
+    })
+});
